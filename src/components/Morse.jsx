22c8f09442086d6cef15071657f8ac0cf33c7dc6
Arohan Dutt <arohandutt@gmail.com>
2019-09-25 18:00:50 -0500

    Add basic func and styling, format pred text

---
+++ src/components/Morse.jsx
@@ -0,0 +1,127 @@
+import React, {Component} from 'react';
+import { useTimer } from 'use-timer';
+import { endianness } from 'os';
+import Button from '@material-ui/core/Button';
+import wordData from '../../db/20k.js'
+
+const morseLib = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9"
+}
+
+class Morse extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          morseMessage: '',
+          message: '',
+          tap_sensitivity: 0.2,
+          space_sensitivity: 1,
+          backspace_sensitivity: 2,
+          allowSpace: false,
+        };
+    }
+
+    translateTime(time) {
+      //console.log(time);
+      let indicator = (time < this.state.tap_sensitivity) ? '.' : '-';
+      this.setState({
+        morseMessage: this.state.morseMessage + indicator,
+      })
+    }
+
+    translate(input){
+      if(input) {
+        let letter = morseLib[input];
+        console.log(letter)
+        let newMessage = this.state.message + letter;
+        this.setState({
+          message: newMessage,
+          morseMessage: ''
+        })
+  
+        return input;
+      }
+    }
+
+    renderSpace() {
+      console.log('flare')
+      this.translate(this.state.morseMessage)
+    }
+
+    render() {
+        return (
+            <div id='morse_container'>
+              <h1 className='header'>Morse</h1>
+              <div className='message_box'>{this.state.message}</div>
+              <div className='input_container'>
+                <div className='message_box morse_current preview_left'>{morseLib[this.state.morseMessage]}</div>
+                <div className='message_box morse_current'>{this.state.morseMessage}</div>
+              </div>
+              <MorseInput translateTime={this.translateTime.bind(this)}
+                          space_sensitivity={this.state.space_sensitivity}
+                          translate={this.translate}
+                          renderSpace={this.renderSpace.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+const MorseInput = (props) => {
+  const { time: charTime, start: charStart, pause: charPause, reset: charReset } = useTimer({interval: 100});
+
+
+  return (
+    <Button variant="contained" id='morse_input'
+         onTouchStart={() => {
+          charStart();
+          window.clearTimeout(window.timeOutID);
+         }}
+         onTouchEnd={() => {
+          charPause();
+          props.translateTime(charTime / 10);
+          charReset();
+
+          window.timeOutID = window.setTimeout( function () {
+            props.renderSpace()
+          }.bind(this), 500)
+         }}
+         >Tap Here</Button>
+  )
+}
+
+export default Morse;
--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home.jsx'
+import Morse from './components/Morse.jsx'
 
 function RouteManager() {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Morse} />
       </div>
     </Router>
   );
