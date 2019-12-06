34966881c426aa86a317143f2b7c079bbe4b9d30
Arohan Dutt <arohandutt@gmail.com>
2019-12-06 12:09:15 -0600

    Refactor Morse for Hooks

--- src/components/Morse.jsx
+++ src/components/Morse.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect, useRef} from 'react';
 import { useTimer } from 'use-timer';
 import Button from '@material-ui/core/Button';
 import wordData from '../../db/20k.js'
@@ -9,61 +9,64 @@ import SpeakerPhone from '@material-ui/icons/SpeakerPhone';
 import CustomDrawer from './Drawer.jsx'
 import {morseLib} from './Dictionary.jsx'
 
-class Morse extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          morseMessage: '',
-          message: '',
-          tap_sensitivity: 0.2,
-          space_sensitivity: 1,
-          predictions: {
-            predictionOne: '',
-            predictionTwo: '',
-            predictionThree: ''
-          },
-          currentWord: '',
-          letterSensitivity: 800,
-          spaceSensitivity: 2000,
-          renderSettings: false
-        };
+const Morse = () => {
+
+    const blankPredictions = {
+      predictionOne: '',
+      predictionTwo: '',
+      predictionThree: ''
+    };
+    const [morseMessage, setMorseMessage] = useState('')
+    const currentMorseMessage = useRef(morseMessage);
+    currentMorseMessage.current = morseMessage;
+    const [message, setMessage] = useState('');
+    const currentMessage = useRef(message);
+    currentMessage.current = message;
+    const [tap_sensitivity, setTap_sensitivity] = useState(0.2);
+    const [space_sensitivity, setSpace_sensitivity] = useState(1);
+    const [predictions, setPredictions] = useState(blankPredictions);
+    const [currentWord, setCurrentWord] = useState('');
+    const [letterSensitivity, setLetterSensitivity] = useState(800);
+    const [spaceSensitivity, setSpaceSensitivity] = useState(2000);
+    const [renderSettings, setRenderSettings] = useState(false);
+
+    useEffect(() => {
+      let lastWord = message.split(' ').reverse()[0].toLowerCase();
+      setCurrentWord(lastWord);
+    }, [morseMessage]);
+    
+    useEffect(() => {
+      updatePredictions(currentWord);
+    }, [currentWord]);
+
+    const translateTime = (time) => {
+      let indicator = (time < tap_sensitivity) ? '.' : '-';
+      let newMorseMessage = morseMessage + indicator;
+      setMorseMessage(newMorseMessage);
     }
 
-    translateTime(time) {
-      let indicator = (time < this.state.tap_sensitivity) ? '.' : '-';
-      this.setState({
-        morseMessage: this.state.morseMessage + indicator,
-      })
-    }
-
-    translate(input){
+    const translate = (input) => {
       if(input) {
         let letter = morseLib[input];
-        let newMessage = this.state.message + letter;
-        this.setState({
-          message: newMessage,
-          morseMessage: ''
-        }, () => {
-          let lastWord = this.state.message.split(' ').reverse()[0].toLowerCase();
-          this.setState({currentWord: lastWord}, this.updatePredictions(lastWord))
-        })
+        let newMessage = message + letter;
+
+        setMessage(newMessage);
+        setMorseMessage('');
   
         return input;
       }
     }
 
-    renderLetter() {
-      this.translate(this.state.morseMessage)
+    const renderLetter = () => {
+      translate(currentMorseMessage.current)
     }
 
-    renderSpaces() {
-      this.setState({
-        message: this.state.message + ' '
-      })
+    const renderSpaces = () => {
+      let newMessage = currentMessage.current + ' ';
+      setMessage(newMessage);
     }
 
-    updatePredictions(string) {
-      console.log(string)
+    const updatePredictions = (string) => {
       let firstPrediction = '', secondPrediction = '', thirdPrediction = '';
       for(let i = 0; i < wordData.length; i++){
         if( (wordData[i][0] === string[0]) && wordData[i].includes(string)){
@@ -77,81 +80,73 @@ class Morse extends Component {
           }
         }
       }
-      this.setState({
-        predictions: {
-          predictionOne: firstPrediction.toUpperCase(),
-          predictionTwo: secondPrediction.toUpperCase(),
-          predictionThree: thirdPrediction.toUpperCase()
-        }
-      })
+      let newPredictions = {
+        predictionOne: firstPrediction.toUpperCase(),
+        predictionTwo: secondPrediction.toUpperCase(),
+        predictionThree: thirdPrediction.toUpperCase()
+      };
+
+      setPredictions(newPredictions);
     }
 
-    selectPrediction(word) {
-      let newMessageArray = this.state.message.split(' ');
+    const selectPrediction = (word) => {
+      let newMessageArray = message.split(' ');
       newMessageArray.pop();
       let newMessage = newMessageArray.join(' ') + ' ' + word + ' ';
-      this.setState({
-        message: newMessage,
-        currentWord: '',
-        predictions: {
-          predictionOne: '',
-          predictionTwo: '',
-          predictionThree: ''
-        },
-      })
+
+      setMessage(newMessage);
+      setCurrentWord('');
+      setPredictions(blankPredictions);
+
     }
 
-    handleBackSpace() {
-      this.setState({
-        message: this.state.message.substring(0, this.state.message.length - 1)
-      })
+    const handleBackSpace = () => {
+      const newMessage = message.substring(0, message.length - 1);
+      setMessage(newMessage);
     }
 
-    handleSpeech() {
-      let utterance = new SpeechSynthesisUtterance(this.state.message);
+    const handleSpeech = () => {
+      let utterance = new SpeechSynthesisUtterance(message);
       window.speechSynthesis.speak(utterance);
     }
 
-    render() {
-        return (
+    return (
 
-          this.state.renderSettings 
-          ? null 
-          :
-            <div id='morse_container'>
-              <h1 className='header'>Morse Interpreter</h1>
-              <div className='message_box main_message'>{this.state.message}</div>
-              <div className='input_container'>
-                <div className='message_box morse_current preview_left'>{morseLib[this.state.morseMessage]}</div>
-                <div className='message_box morse_current'>{this.state.morseMessage}</div>
-              </div>
-              <Predictions predictions={this.state.predictions} 
-                          selectPrediction={this.selectPrediction.bind(this)}/>
-              <div id='input_container'>
-                <MorseInput translateTime={this.translateTime.bind(this)}
-                            space_sensitivity={this.state.space_sensitivity}
-                            translate={this.translate}
-                            renderLetter={this.renderLetter.bind(this)}
-                            renderSpaces={this.renderSpaces.bind(this)}
-                            letterSensitivity={this.state.letterSensitivity}
-                            spaceSensitivity={this.state.spaceSensitivity}/>
-                <div id='alternate_inputs'>
-                  <CustomDrawer />
-                  <Button id='green_button' variant="contained"
-                          onClick={this.handleSpeech.bind(this)}><SpeakerPhone /></Button>
-                  <Button id='red_button' variant="contained"
-                          onClick={this.handleBackSpace.bind(this)}><Backspace /></Button>
-                </div>
-              </div>
+      renderSettings 
+      ? null 
+      :
+        <div id='morse_container'>
+          <h1 className='header'>Morse Interpreter</h1>
+          <div className='message_box main_message'>{message}</div>
+          <div className='input_container'>
+            <div className='message_box morse_current preview_left'>{morseLib[morseMessage]}</div>
+            <div className='message_box morse_current'>{morseMessage}</div>
+          </div>
+          <Predictions predictions={predictions} 
+                      selectPrediction={selectPrediction.bind(this)}/>
+          <div id='input_container'>
+            <MorseInput translateTime={translateTime.bind(this)}
+                        space_sensitivity={space_sensitivity}
+                        translate={translate}
+                        renderLetter={renderLetter.bind(this)}
+                        renderSpaces={renderSpaces.bind(this)}
+                        letterSensitivity={letterSensitivity}
+                        spaceSensitivity={spaceSensitivity}/>
+            <div id='alternate_inputs'>
+              <CustomDrawer />
+              <Button id='green_button' variant="contained"
+                      onClick={handleSpeech.bind(this)}><SpeakerPhone /></Button>
+              <Button id='red_button' variant="contained"
+                      onClick={handleBackSpace.bind(this)}><Backspace /></Button>
             </div>
-        )
-    }
+          </div>
+        </div>
+    )
 }
 
 const MorseInput = (props) => {
   const { time: charTime, start: charStart, pause: charPause, reset: charReset } = useTimer({interval: 100});
   const settings = JSON.parse(localStorage.getItem('vocal_codes_settings'));
-  console.log(settings)
 
   return (
     <Button variant="contained" id='morse_input'
